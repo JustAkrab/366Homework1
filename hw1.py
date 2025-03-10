2e4c1a2a6853694e7bc17c4ac51c434cebc47cf8
JustAkrab <reda.akrab13@gmail.com>
2025-03-10 06:17:58 -0400

    reader

--- hw1.py
+++ hw1.py
@@ -2,24 +2,9 @@
 
 
 class hw1():
-    
-    
-    def reader_train():
-        # Pad each sentence in the training and test corpora with start and end symbols (you can
-        # use <s> and </s>, respectively).
-    
-    
-        # To begin, start with a reader function for each of the files given
-        return None
-    
-    def writer_train():
-        return None
-        
-    def algorithm():
-        return None
-    
-    def main():
-        return 0
+    with open('train-Spring2025.txt', 'r', encoding="utf8") as f:
+        for line in f:
+            print(line)
     
         
     
