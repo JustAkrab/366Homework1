c033c8aeb39b474337adb1c34ed400af9f9c13a0
Reda <reda.akrab13@gmail.com>
2025-03-10 18:41:58 -0400

    Finished lowercase, unk, bigram/unigram logic

--- hw1.py
+++ hw1.py
@@ -1,17 +1,26 @@
 
 
 
-# dictionary_one = {}
+dictionary_one = {}
 padded_list = []
 with open('train-Spring2025.txt', 'r', encoding="utf8") as f:
     for line in f:
             # print(line)
+            # padding
         sen = "<s> " + line.rstrip() + " </s>"
         spit = sen.split()
+        # apply each word to be lowercase
         for i in range(len(spit)):
             # print(i.lower())
             spit[i] = spit[i].lower()
-        print(spit)
+            
+            dictionary_one[spit[i]] = dictionary_one.get(spit[i], 0) + 1 
+        # print(spit)
+
+        # Replace all words occurring in the training data once with the token <unk>. Every word
+        # in the test data not seen in training should be treated as <unk>
+
+
 
         # print(spit)
         
@@ -19,15 +28,33 @@ with open('train-Spring2025.txt', 'r', encoding="utf8") as f:
 
 with open('test.txt', 'r') as tf:
     for rine in tf:
-        sin = "<s> " + line.rstrip() + " </s>"
+        sin = "<s> " + rine.rstrip() + " </s>"
         sprint = sin.split()
+        # print(sprint)
+        for j in range(len(sprint)):
+            sprint[j] = sprint[j].lower()
         print(sprint)
+
 
 
 
     
     # with open('train-Spring2025.txt', 'r', encoding="utf8") as wf:
     #     for write in 
+
+
+# bigram logic
+
+
+
+#unigram logic
+
+
+
+
+# trigram logic
+
+
 
             
             
