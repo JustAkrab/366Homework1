f48314d3a15e95a05f7e67aef6ef5c6b7334edd6
Reda <reda.akrab13@gmail.com>
2025-03-10 17:31:44 -0400

    Finished part 1.1.1, and 1.1.2

--- hw1.py
+++ hw1.py
@@ -1,10 +1,36 @@
 
 
 
-class hw1():
-    with open('train-Spring2025.txt', 'r', encoding="utf8") as f:
-        for line in f:
-            print(line)
+# dictionary_one = {}
+padded_list = []
+with open('train-Spring2025.txt', 'r', encoding="utf8") as f:
+    for line in f:
+            # print(line)
+        sen = "<s> " + line.rstrip() + " </s>"
+        spit = sen.split()
+        for i in range(len(spit)):
+            # print(i.lower())
+            spit[i] = spit[i].lower()
+        print(spit)
+
+        # print(spit)
+        
+
+
+with open('test.txt', 'r') as tf:
+    for rine in tf:
+        sin = "<s> " + line.rstrip() + " </s>"
+        sprint = sin.split()
+        print(sprint)
+
+
+
+    
+    # with open('train-Spring2025.txt', 'r', encoding="utf8") as wf:
+    #     for write in 
+
+            
+            
     
         
     
