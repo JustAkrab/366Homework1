c92786a1cb1ef499e59188fc23b1828f02893c1c
JustAkrab <reda.akrab13@gmail.com>
2025-03-10 05:54:18 -0400

    Init Commit

---
+++ hw1.py
@@ -0,0 +1,25 @@
+
+
+
+class hw1():
+    
+    
+    def reader_train():
+        # Pad each sentence in the training and test corpora with start and end symbols (you can
+        # use <s> and </s>, respectively).
+    
+    
+        # To begin, start with a reader function for each of the files given
+        return None
+    
+    def writer_train():
+        return None
+        
+    def algorithm():
+        return None
+    
+    def main():
+        return 0
+    
+        
+    
